2eca371c411b191100d6279386e27a004819a45c
irfancahhyo11 <irfancahhyo11@gmail.com>
2025-07-15 09:15:09 +0000

    Update rurima-cli.py

--- rurima-cli.py
+++ rurima-cli.py
@@ -1,9 +1,20 @@
 import os
 import subprocess
+import time
+import sys
+if sys.stdin.encoding.lower() != 'utf-8':
+    import codecs
+    sys.stdin = codecs.getreader('utf-8')(sys.stdin.detach())
+    sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
 
 print("installing rurima")
 
 os.system("curl -sL https://get.ruri.zip/rurima | bash > /dev/null")
+
+time.sleep(1)
+
+print("\n\n")
+
 while True:
 	print("rurima-cli \n select \n 1. Docker \n 2. LXC \n 3. Pull \n 4. Exit \n select number")
 	num = input("> ")
@@ -14,13 +25,17 @@ while True:
 		docker_tag = input("> ")
 		print("Input docker image name (to save):")
 		docker_path = input("> ")
-		
-		command = f"rurima docker pull -i {docker_name} -t {docker_tag} -s ./{docker_path} > {docker_path}_log.sh"
 
-		os.system(command)
-		
+		print("Pulling image!")
 		
 
+		command = f"rurima docker pull -i {docker_name} -t {docker_tag} -s ./{docker_path}"
+		with open(f"{docker_path}_log.sh", "w") as f:
+			process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+			for line in process.stdout:
+				print(line, end='')
+				f.write(line)
+			process.wait()
 		print(f"Image pulled. to get start command check {docker_path}_log.sh")
 		
 	elif num == "2":
@@ -41,10 +56,13 @@ while True:
 			print("Input LXC image name (to save):")
 			lxc_image_path = input("> ")
 			
-			command = f"rurima lxc pull -i {lxc_image_name} -t {lxc_image_tag} -s ./{lxc_image_path} > {lxc_image_path}_log.sh"
-			
-			os.system(command)
-			
+			command = f"rurima lxc pull -i {lxc_image_name} -t {lxc_image_tag} -s ./{lxc_image_path}"
+			with open(f"{lxc_image_path}_log.sh", "w") as f:
+				process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+				for line in process.stdout:
+					print(line, end='')
+					f.write(line)
+				process.wait()
 			print(f"Image pulled. to get start command check {lxc_image_path}_log.sh")
 			continue
 		
@@ -52,12 +70,14 @@ while True:
 		print("Pull option selected")
 		print("Input what to pull:")
 		pull_target = input("> ")
-		command = f"rurima pull {pull_target} > {pull_target}_log.sh"
-		
-		os.system(command)
-		
-		print(f"Command executed. Log saved to pull_log.sh")
-		print(f"Return code: {result.returncode}")
+		command = f"rurima pull {pull_target}"
+		with open(f"{pull_target}_log.sh", "w") as f:
+			process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+			for line in process.stdout:
+				print(line, end='')
+				f.write(line)
+			process.wait()
+		print(f"Command executed. Log saved to {pull_target}_log.sh")
 		
 	elif num == "4":
 		print("Exiting...")
